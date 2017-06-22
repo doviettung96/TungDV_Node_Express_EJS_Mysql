9b47012ae61ec2cb73181998f88c0b2b95687dce
doviettung96 <doviettung96@github.com>
2017-06-22 16:16:11 +0700

    update version

--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ -z "2a135cb8949cdf4bd683a3b9f513e384a135f2f6" ]; then
+if [ -z "$GH_TOKEN" ]; then
     echo "You must set the GH_TOKEN environment variable."
     echo "See README.md for more details."
     exit 1
 fi
 
 # This will build, package and upload the app to GitHub.
-node_modules/.bin/build --win --mac -p always
+node_modules/.bin/build --win -p always
